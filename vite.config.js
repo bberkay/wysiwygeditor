6212d42f36dcc02d8755143bbe4a1b16b0cce4a5
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-29 03:59:31 +0300

    index.d.ts fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [
+    cssInjectedByJsPlugin(),
+    copy({
+      targets: [{ src: 'lib/index.d.ts', dest: 'dist', rename: 'wysiwygeditor.d.ts' }]
+    })
+  ],
   build:{
       lib: {
         entry: resolve(__dirname, 'lib/index.js'),
