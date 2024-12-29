c5c7af3eb6864657ff10e189908295b0f4f663ad
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-29 21:25:57 +0300

    version-hotfix

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   plugins: [
-    cssInjectedByJsPlugin(),
-    copy({
-      targets: [{ src: 'lib/index.d.ts', dest: 'dist', rename: 'wysiwygeditor.d.ts' }]
-    })
+    cssInjectedByJsPlugin()
   ],
   build:{
       lib: {
         entry: resolve(__dirname, 'lib/index.js'),
         name: 'WYSIWYGEditor',
-        filename: 'wysiwygeditor',
+        fileName: (format) => `wysiwygeditor.${format}.js`,
       },
       rollupOptions: {},
       minify: 'esbuild',
